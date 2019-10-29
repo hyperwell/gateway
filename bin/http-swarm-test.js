0efdcfb5fe8ea320fae434fa7e0a23d346832484
Jan Kaßel <jan@kassel.works>
2019-10-29 20:14:45 +0100

    add preliminary keep-alive support

--- bin/http-swarm-test.js
+++ bin/http-swarm-test.js
@@ -1,38 +1,36 @@
 #!/usr/bin/env node
-const Hyperswarm = require('hyperswarm');
-const crypto = require('crypto');
-const {createRequest} = require('../lib/http-request');
-const {createStore} = require('../lib/repo-store');
+const Hyperswarm = require('hyperswarm')
+const crypto = require('crypto')
+const {createRequest} = require('../lib/http-request')
+const {createStore} = require('../lib/repo-store')
 
 async function main() {
-  const repoStore = await createStore();
-  const docUrl = repoStore.repos[0].docs[0];
+  const repoStore = await createStore()
+  const docUrl = repoStore.repos[0].docs[0]
 
-  const swarm = Hyperswarm();
+  const swarm = Hyperswarm()
 
-  console.log(`Joining second swarm: annotations-${docUrl}`);
+  console.log(`Joining second swarm: annotations-${docUrl}`)
   const topic = crypto
     .createHash('sha256')
     .update(`annotations-${docUrl}`)
-    .digest();
+    .digest()
 
   swarm.join(topic, {
     lookup: true,
     announce: false,
-  });
+  })
 
   await new Promise(resolve => {
     swarm.once('connection', async socket => {
-      await createRequest(
-        socket,
-        `/annotations/${encodeURIComponent(docUrl)}.jsonld`
-      );
-      resolve();
-    });
-  });
+      await createRequest(socket, `/annotations.jsonld`, true)
+      await createRequest(socket, `/related.json`)
+      resolve()
+    })
+  })
 
-  console.log('Leaving swarm...');
-  swarm.destroy();
+  console.log('Leaving swarm...')
+  swarm.destroy()
 }
 
-main();
+main()
--- client.js
+++ client.js
@@ -1,46 +1,59 @@
-#!/usr/bin/env node
-const {Repo} = require('hypermerge');
-const Hyperswarm = require('hyperswarm');
-const {createStore} = require('./lib/repo-store');
-const {serveSwarm} = require('./lib/http-swarm');
+const {Repo} = require('hypermerge')
+const Hyperswarm = require('hyperswarm')
+const uuid = require('uuid/v1')
+const {createStore} = require('./lib/repo-store')
+const {serveSwarm} = require('./lib/http-swarm')
+// const {announceDocument, unannounceAll} = require('./lib/related-swarm')
 
 async function main() {
-  const repoStore = await createStore();
-  const repoConf = repoStore.repos[0];
-  const {id} = repoConf;
+  const repoStore = await createStore()
+  let repo, id, repoConf
 
-  console.log(`Reading repository with id: ${id}`);
-  const repo = new Repo({path: `${__dirname}/../.data/${id}`});
+  if (repoStore.repos.length === 0) {
+    const id = uuid()
+    repo = new Repo({path: `${__dirname}/../.data/${id}`})
+    repoConf = await repoStore.addRepo(id, repo)
+    id = repoConf.id
+  } else {
+    repoConf = repoStore.repos[0]
+    id = repoConf.id
+    repo = new Repo({path: `${__dirname}/../.data/${id}`})
+  }
 
-  repo.setSwarm(Hyperswarm());
-  console.log('Joined swarm.');
+  console.log(`Read repository with id: ${id}`)
+
+  repo.setSwarm(Hyperswarm())
+  console.log('Joined swarm.')
 
   if (repoConf.docs.length === 0) {
-    const url = repo.create({hello: 'world'});
-    console.log(await repo.doc(url));
+    const url = repo.create({hello: 'world'})
+    console.log(await repo.doc(url))
 
-    await repoStore.addDoc(id, url);
+    await repoStore.addDoc(id, url)
   } else {
-    console.log(`Watching doc: ${repoConf.docs[0]}`);
+    console.log(`Watching doc: ${repoConf.docs[0]}`)
 
     const handles = await Promise.all(
       repoConf.docs.map(docUrl =>
         repo.watch(docUrl, () => console.log(`Update at: ${docUrl}`))
       )
-    );
+    )
 
-    const leaveSwarm = serveSwarm(repo, repoConf.docs[0]);
+    const leaveSwarm = serveSwarm(repo, repoConf.docs[0])
+    // await announceDocument('foo-bar-example', repoConf.docs[0]);
 
     process.on('SIGINT', async () => {
-      console.log('Closing watchers...');
+      console.log('Closing watchers...')
 
-      await leaveSwarm();
-      handles.forEach(handle => handle.close());
-      repo.close();
+      // await unannounceAll();
 
-      process.exit(0);
-    });
+      await leaveSwarm()
+      handles.forEach(handle => handle.close())
+      repo.close()
+
+      process.exit(0)
+    })
   }
 }
 
-main();
+main()
