769143ab5d1343f032d0aefd8537428cfc4f17b5
Jan Kaßel <jan@kassel.works>
2019-11-04 00:40:10 +0100

    fix browser example lib import

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -1,4 +1,4 @@
-const {RequestSwarm} = require('from-me-to-you/dist/me2u')
+const {RequestSwarm} = require('from-me-to-you/browser')
 
 const docUrl = window.location.hash.substr(1)
 console.log(`joining request swarm with doc url: ${docUrl}`)
