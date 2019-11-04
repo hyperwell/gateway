575d59ee07c879eb43a901d944004f83706dc6d7
Jan Kaßel <jan@kassel.works>
2019-11-04 20:23:14 +0100

    update examples, adapt to hyperswarm-ws changes

--- browser-bundle.js
+++ browser-bundle.js
@@ -1,2 +1,5 @@
+require('core-js/stable')
+require('regenerator-runtime/runtime')
+
 const {createRequestSwarm} = require('./lib/request/browser-swarm')
 module.exports = {createRequestSwarm}
--- examples/browser/index.js
+++ examples/browser/index.js
@@ -1,11 +1,19 @@
-const {RequestSwarm} = require('from-me-to-you/browser')
+require('core-js/stable')
+require('regenerator-runtime/runtime')
+const {createRequestSwarm} = require('from-me-to-you/browser')
 
-const docUrl = window.location.hash.substr(1)
-console.log(`joining request swarm with doc url: ${docUrl}`)
+async function main() {
+  const docUrl = window.location.hash.substr(1)
+  console.log(`joining request swarm with doc url: ${docUrl}`)
 
-new RequestSwarm(docUrl, {
-  onConnection: async createRequest => {
-    console.log(await createRequest('GET', '/annotations.jsonld'))
-    console.log(await createRequest('GET', '/related.json'))
-  },
-})
+  const RequestSwarm = await createRequestSwarm()
+
+  new RequestSwarm(docUrl, {
+    onConnection: async createRequest => {
+      console.log(await createRequest('GET', '/annotations.jsonld'))
+      console.log(await createRequest('GET', '/related.json'))
+    },
+  })
+}
+
+main()
