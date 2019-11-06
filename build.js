3fda18c3b745583dc79c92e9905062c0c4b7b69e
Jan Kaßel <jan@kassel.works>
2019-11-06 19:22:46 +0100

    improve buildchain with mkdirp, sanitizing
    don't include all package.json information, see
    <https://github.com/indutny/elliptic/issues/106>

--- build.js
+++ build.js
@@ -1,13 +1,20 @@
 const fs = require('fs')
+const {parse: parsePath} = require('path')
+const mkdirp = require('mkdirp')
 const browserify = require('browserify')
+const sanitizeManifest = require('browserify-package-json')
 
 const babelConf = JSON.parse(fs.readFileSync(`${__dirname}/.babelrc`, 'utf8'))
+const targetPath = process.argv.length > 2 ? process.argv[2] : null
+
+if (targetPath !== null) {
+  mkdirp.sync(parsePath(targetPath).dir)
+}
 const target =
-  process.argv.length > 2
-    ? fs.createWriteStream(process.argv[2])
-    : process.stdout
+  targetPath !== null ? fs.createWriteStream(process.argv[2]) : process.stdout
 
 browserify(`${__dirname}/browser-bundle.js`)
+  .transform({global: true}, sanitizeManifest)
   .transform('babelify', {
     ...babelConf,
     global: true,
