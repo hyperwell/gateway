059e4047a8d364f0478bd4024a9d669dbbfde8d1
Jan Kaßel <jan@kassel.works>
2019-11-13 23:53:21 +0100

    add example error handler

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -6,7 +6,11 @@ async function main() {
   const docUrl = window.location.hash.substr(1)
   console.log(`joining request swarm with doc url: ${docUrl}`)
 
-  const swarm = new RequestSwarm(docUrl)
+  const swarm = new RequestSwarm(docUrl, {
+    handleError: function(err) {
+      console.error('Bad error:', err)
+    },
+  })
   await new Promise(resolve => swarm.on('ready', resolve))
 
   console.log(await swarm.getAnnotations())
