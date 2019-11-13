2764ddf67bfc2d037dbb736c149c6e540cb11f0e
Jan Kaßel <jan@kassel.works>
2019-11-13 14:09:04 +0100

    add example sourcemaps, update request API

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -1,19 +1,16 @@
 require('core-js/stable')
 require('regenerator-runtime/runtime')
-const {createRequestSwarm} = require('from-me-to-you/browser')
+const {RequestSwarm} = require('from-me-to-you/browser')
 
 async function main() {
   const docUrl = window.location.hash.substr(1)
   console.log(`joining request swarm with doc url: ${docUrl}`)
 
-  const RequestSwarm = await createRequestSwarm()
+  const swarm = new RequestSwarm(docUrl)
+  await new Promise(resolve => swarm.on('ready', resolve))
 
-  new RequestSwarm(docUrl, {
-    onConnection: async createRequest => {
-      console.log(await createRequest('GET', '/annotations.jsonld'))
-      console.log(await createRequest('GET', '/related.json'))
-    },
-  })
+  console.log(await swarm.getAnnotations())
+  console.log(await swarm.getRelated())
 }
 
 main()
