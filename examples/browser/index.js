4c5df63868490c94b9ee3afa134d6b249896c96a
Jan Kaßel <jan@kassel.works>
2019-11-14 15:31:34 +0100

    add new IDs, pub-sub architecture
    lots of changes. requests now may provide their own UUIDs--responses
    will include that ID, as we are sending all messages over the same
    connection. also, the protocol now supports experimental pub-sub
    messages for real-time updates. a respective method for subscribing to
    all annotations of a notebook is provided for client swarms.

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -6,6 +6,7 @@ async function main() {
   const docUrl = window.location.hash.substr(1)
   console.log(`joining request swarm with doc url: ${docUrl}`)
 
+  // initialize swarm, wait until it's ready
   const swarm = new RequestSwarm(docUrl, {
     handleError: function(err) {
       console.error('Bad error:', err)
@@ -13,8 +14,29 @@ async function main() {
   })
   await new Promise(resolve => swarm.on('ready', resolve))
 
-  console.log(await swarm.getAnnotations())
-  console.log(await swarm.getRelated())
+  // get current peer state
+  console.log(`Current annotations:
+${JSON.stringify(await swarm.getAnnotations(), null, 2)}`)
+  console.log(`Related documents to target:
+${JSON.stringify(await swarm.getRelated(), null, 2)}`)
+
+  // create a subscription
+  const subscription = await swarm.getAnnotations({subscribe: true})
+  subscription.on('pub', data =>
+    console.log(`PUB:
+${JSON.stringify(data, null, 2)}`)
+  )
+  subscription.on('close', () => console.log('PUB closed.'))
+  subscription.on('error', err => console.error('PUB error:', err))
+
+  // trigger subscription with a write action
+  setTimeout(
+    () =>
+      swarm.createAnnotation({
+        foo: 'bar',
+      }),
+    2500
+  )
 }
 
 main()
